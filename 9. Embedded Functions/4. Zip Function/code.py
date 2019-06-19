adf8726951ca4461db2d97ce98c551e5b03d09d6
Sefa İşci <sefa314159@gmail.com>
2019-06-19 16:50:20 +0300

    Create code.py

---
+++ 9. Embedded Functions/4. Zip Function/code.py
@@ -0,0 +1,4 @@
+names=["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+surnames=["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+for i,j in zip(isimler,soyisimler):
+    print("{} {}".format(i,j))
