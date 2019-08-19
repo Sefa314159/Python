7d422c14812c032361b96cf60f49169ac6588065
Sefa İşci <sefa314159@gmail.com>
2019-08-19 23:02:00 +0300

    Update code.py

--- 18. Os Module/code.py
+++ 18. Os Module/code.py
@@ -4,7 +4,7 @@ file1 = open("pdf_dosyaları.txt", "w", encoding = "utf-8")
 file2 = open("mp4_dosyaları.txt", "w", encoding = "utf-8")
 file3 = open("txt_dosyaları.txt", "w", encoding = "utf-8")
 
-for folder_path, folder_name, file_name in os.walk("C:/Users/user/Desktop"):
+for folder_path, folder_name, file_name in os.walk("C:/Users/Sefa3/Desktop"):
     for i in file_name:
         if(i.endswith(".pdf")):
             file1.write("{}\n".format(i))
